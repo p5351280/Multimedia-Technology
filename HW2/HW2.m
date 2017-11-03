0ef1ce8b6440d58ac0117e492c0e19168e6930e5
p5351280 <p5351280@gmail.com>
2017-11-03 20:09:49 +0800

    speed up

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -3,34 +3,46 @@ clc;
 
 x = linspace(-2, 2, 500);
 y = linspace(-2, 2, 500);
-C = [];
-origin = [];
+C = zeros(1, 250000);
+insideCount = 0;
 for i = 1:500
     for j = 1:500
         if(x(i)^2+y(j)^2 <= 4)
-            C = [C complex(x(i), y(j))];
+            insideCount = insideCount+1;
+            C(insideCount) = complex(x(i), y(j));
         end
     end
 end
+C(C(:)==0) = [];
 origin = C;
+insideCount = length(C);
 plot(C, '.', 'MarkerEdgeColor', 'k');
 
 for iterativeCount = 1:20
     hold on;
-    outside = [];
-    inside = [];
-    originNew = [];
-    for i = 1:length(C)
+    len = insideCount;
+    outside = zeros(1, 250000);
+    outsideCount = 0;
+    inside = zeros(1, 250000);
+    originNew = zeros(1, 250000);
+    insideCount = 0;
+    
+    for i = 1:len
         result = Mandelbrot(C(i), origin(i));
         if(abs(result) > 2)
-            outside = [outside origin(i)];
+            outsideCount = outsideCount+1;
+            outside(outsideCount) = origin(i);
         else
-            inside = [inside result];
-            originNew = [originNew origin(i)];
+            insideCount = insideCount+1;
+            inside(insideCount) = result;
+            originNew(insideCount) = origin(i);
         end
     end
-    C = inside;
+    outside(outside(:)==0) = [];
+    inside(inside(:)==0) = [];
+    originNew(originNew(:)==0) = [];
     origin = originNew;
+    C = inside;
     if mod(iterativeCount, 3) == 1
         plot(outside, '.', 'MarkerEdgeColor', 'r');
     elseif mod(iterativeCount, 3) == 2
